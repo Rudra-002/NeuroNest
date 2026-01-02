70b265bdeeb8e8afece54c4ca5416cd86dd7f655
Rudra-002 <hell.rider.0510@gmail.com>
2026-01-03 01:05:43 +0530

    NeuroNest

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ questions.forEach((q, index) => {
     q6: Number(document.getElementById("q6").value)
   };
 
-  const response = await fetch("http://127.0.0.1:3000/analyze", {
+  const response = await fetch("https://neuronest-backend-faz3.onrender.com/analyze"
+, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload)
